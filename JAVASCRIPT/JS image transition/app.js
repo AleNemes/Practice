caf193205dc2c0e45cfafd60a44de6ae899073f7
AleNemes <64965844+AleNemes@users.noreply.github.com>
2021-01-24 22:12:20 +0200

    fullpage

---
+++ JAVASCRIPT/JS image transition/app.js
@@ -0,0 +1,8 @@
+new hoverEffect ({
+  parent: document.querySelector('.distorsion'),
+  intensity: 0.2,
+  image1: 'img/amd360c.jpg',
+  image2: 'img/amd360.jpg',
+  angle: Math.PI / 4,
+  displacementImage: 'img/strip.png'
+})
